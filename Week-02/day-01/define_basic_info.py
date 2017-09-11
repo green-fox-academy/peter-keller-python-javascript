c6ea24860e8efe0eec8492cee3779e3818be719c
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-11 14:15:38 +0200

    finished exercise 10

--- Week-02/day-01/define_basic_info.py
+++ Week-02/day-01/define_basic_info.py
@@ -1,5 +1,13 @@
 # Define several things as a variable than print their values
 # Your name as a string
+name = "Peti"
+print(name)
 # Your age as an integer
+age = 23
+print(age)
 # Your height in meters as a double
+height = 1.74
+print(height)
 # Wether you are married or not as a boolean
+is_married = False
+print(is_married)
