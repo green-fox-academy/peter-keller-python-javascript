384d5141a6b5e6973b12c8bdd49326e2f03340e1
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-11 15:46:21 +0200

    finished exercise 17

--- Week-02/day-01/average_of_input.py
+++ Week-02/day-01/average_of_input.py
@@ -2,3 +2,12 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+iso = int(input())
+inpo = int(input())
+ipo = int(input())
+inpu = int(input())
+inp = int(input())
+Sum=iso + inpo + ipo + inpu + inp
+print("Sum:" + str(Sum))
+Average = Sum / 5
+print("Average:" + str(Average))
