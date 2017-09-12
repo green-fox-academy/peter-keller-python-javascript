4fb38fc19280ebaa80fba4de3e3d4b2f60dd6942
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-12 11:41:42 +0200

    finished exercise 5

---
+++ Week-02/day-02/factorio.py
@@ -0,0 +1,12 @@
+# - Create a function called `factorio`
+#   that returns it's input's factorial
+n = int(input("Insert number: "))
+
+def factorio(num):
+    g = 1
+    for i in range(1,(num+1)):
+        g = g * i
+    print("Factorio: " ,g)
+
+factorio(n)
+
