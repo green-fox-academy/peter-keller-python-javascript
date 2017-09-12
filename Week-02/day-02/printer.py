3a8fcedca625ee9eb15b4f062888160052b64943
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-12 11:59:32 +0200

    finished exercise 6

---
+++ Week-02/day-02/printer.py
@@ -0,0 +1,7 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+def printer(parameter):
+    print (parameter)
+
+printer("Peti")
