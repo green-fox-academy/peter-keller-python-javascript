8a4ed66bc107cd50d254fe617f77dd269ae250fe
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-12 16:38:36 +0200

    finished exercise 29

--- Week-02/day-01/draw_pyramid.py
+++ Week-02/day-01/draw_pyramid.py
@@ -8,3 +8,11 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+number = int(input("Insert number: "))
+star = "*"
+space = " "
+m = 0
+for n in range (1 + (number+1)):
+    if n % 2 == 1 and n != 0:
+            print(space*(number//2-m) + star*(n))
+            m += 1
