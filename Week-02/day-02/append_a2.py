a6bc48e758f37555f5be59d1eb556b5e357398fd
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-12 14:55:37 +0200

    finished exercise 16

---
+++ Week-02/day-02/append_a2.py
@@ -0,0 +1,10 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macsk", "cic"]`
+# - Add all elements an `"a"` at the end
+
+li = ["kuty", "macsk", "cic"]
+    
+for i in range(0,len(li)):
+    li[i] +="a"
+    print(li[i])
+
