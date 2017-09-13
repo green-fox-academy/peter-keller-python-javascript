a0c7890a338f3de95fa3c353741b15fa9a27939a
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-13 11:00:10 +0200

    feat(reverse): complete

---
+++ Week-02/day-03/reverse.py
@@ -0,0 +1,10 @@
+# Create a function called 'reverse_string' which takes a string as a parameter
+# The function reverses it and returns with the reversed string
+
+
+reverse = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+def reverse_string(reversed):
+    n = reversed[::-1]
+    print(n)
+
+reverse_string(reverse)
