73754853ba79a03a3f687214475c438c16de349e
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-13 12:12:09 +0200

    feat(url_fixer): complete

--- Week-02/day-03/test.py
+++
@@ -1,8 +0,0 @@
-# Accidentally I messed up this quote from Richard Feynman.
-# Two words are out of place
-# Your task is to fix it by swapping the right words with code
-
-# Also, print the sentence to the output with spaces in between.
-
-words = ["What", "I", "do", "create,", "I", "cannot", "not", "understand."]
-print(words.replace("do","cannot"))
