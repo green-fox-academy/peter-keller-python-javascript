964cc2cf445eb9a73c620cb354ff3f1def773cfc
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-14 11:22:47 +0200

    feat(parametric_average): complete

--- Week-02/day-01/parametric_average.py
+++ Week-02/day-01/parametric_average.py
@@ -4,3 +4,10 @@
 # integers like:
 #
 # Sum: 22, Average: 4.4
+number = int(input("Amount of numbers: "))
+sum = 0
+for i in range (0, number):
+    sum += int(input("Number: "))
+print ("Sum: " + str(sum) +  " , Average: " + str(sum / number))
+
+
