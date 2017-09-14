e830088b3d2405c35b987f1bbba3681f2d2b2bb8
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-14 10:14:24 +0200

    feat(draw_diamond): complete

--- Week-02/day-01/draw_diamond.py
+++ Week-02/day-01/draw_diamond.py
@@ -11,3 +11,31 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+rows = int(input("Number of rows: "))
+stars = ""
+spaces = ""
+j = 1
+
+for i in range(0, rows):
+    for k in range(0, j):
+        stars += "*"     
+    for k in range(0, rows - i):
+        spaces += " "
+    print (spaces + stars + "\n")
+    stars = ""
+    spaces = ""
+    j += 2
+
+j = 2 * rows - 3
+spaces = ""
+stars = ""
+
+for i in range(0, rows):
+    for k in range(0, j):
+        stars += "*"     
+    for k in range(0, i+2):
+        spaces += " "
+    print (spaces + stars + "\n")
+    stars = ""
+    spaces = ""
+    j -= 2
