5a163801fdc2f7cd61939b088f4ed9e22f59de81
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-20 11:36:32 +0200

    feat(diagonals): complete

---
+++ Week-03/day-03/diagonals.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+diagonal = canvas.create_line(0, 0, 300, 300, fill="green")
+# draw the canvas' diagonals in green.
+
+root.mainloop()
