aa9cf1164afc6075ffb6a63961c4733ec4791afa
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-22 16:34:38 +0200

    feat(sierpinsky_carpet): fixed order of numbers

--- Week-03/day-05/sierpinsky_carpet.py
+++ Week-03/day-05/sierpinsky_carpet.py
@@ -6,27 +6,29 @@ root = Tk()
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-def carpet(a, x, y):
+def carpet(x, y, a):
     time.sleep(0.0001)
     canvas.update()
-    color_scale =["red", "blue", "grey", "orange", "yellow", "green", "pink", "lime green", "olive drab", "light sea green", "purple", "cyan", "magenta" ]
-    if a < 5:
-        return 0
+    
+    if a < 0:
+        return
     else: 
-        rectangle = canvas.create_rectangle(x+a/3, y+a/3, x+a/3*2, y+a/3*2, fill = random.choice(color_scale))
+        r = lambda: random.randint(0,255)
+        random_color = '#%02X%02X%02X' % (r(),r(),r())
+        rectangle = canvas.create_rectangle(x+a/3, y+a/3, x+a/3*2, y+a/3*2, fill = random_color)
         
-        carpet(a/3, x, y)
-        carpet(a/3, x+a/3, y)
-        carpet(a/3, x+a/3*2, y)
+        carpet(x, y, a/3)
+        carpet(x+a/3, y, a/3)
+        carpet(x+a/3*2, y, a/3)
 
-        carpet(a/3, x, y+a/3)
-        carpet(a/3, x+a/3*2, y+a/3)
+        carpet(x, y+a/3, a/3)
+        carpet(x+a/3*2, y+a/3, a/3)
 
-        carpet(a/3, x, y+a/3*2)
-        carpet(a/3, x+a/3, y+a/3*2)
-        carpet(a/3, x+a/3*2, y+a/3*2)
+        carpet(x, y+a/3*2, a/3)
+        carpet(x+a/3, y+a/3*2, a/3)
+        carpet(x+a/3*2, y+a/3*2, a/3)
         
 
-carpet(600, 0, 0)
+carpet(0, 0, 600)
 
 root.mainloop()
