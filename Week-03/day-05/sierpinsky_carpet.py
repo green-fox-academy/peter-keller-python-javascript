c55aacc5984b1b9fe3ecd8186aa7027d748c96a1
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-22 11:17:16 +0200

    feat(sierpinsky_carpet): added random color

--- Week-03/day-05/sierpinsky_carpet.py
+++ Week-03/day-05/sierpinsky_carpet.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 import time
 root = Tk()
 
@@ -8,11 +9,12 @@ canvas.pack()
 def carpet(a, x, y):
     time.sleep(0.0001)
     canvas.update()
+    color_scale =["red", "blue", "grey", "orange", "yellow", "green", "pink", "lime green", "olive drab", "light sea green", "purple", "cyan", "magenta" ]
     if a < 5:
         return 0
     else: 
-        rectangle = canvas.create_rectangle(x+a/3, y+a/3, x+a/3*2, y+a/3*2, fill="black")
-
+        rectangle = canvas.create_rectangle(x+a/3, y+a/3, x+a/3*2, y+a/3*2, fill = random.choice(color_scale))
+        
         carpet(a/3, x, y)
         carpet(a/3, x+a/3, y)
         carpet(a/3, x+a/3*2, y)
