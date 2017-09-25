9db74ed3fb5270e67fb6992c64b7148391dcb43d
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-25 13:43:35 +0200

    feat(animal): fixed indentation

--- Week-04/day-01/animals.py
+++ Week-04/day-01/animals.py
@@ -6,18 +6,18 @@ class Animal(object):
         self.thirst = 50
     
     def drink(self):
-            self.thirst += 1
-            return print("Thirst: " + str(self.thirst))
+        self.thirst += 1
+        return print("Thirst: " + str(self.thirst))
     
     def eat(self):
-            self.hunger += 1
-            return print("Hunger: " + str(self.hunger))
+        self.hunger += 1
+        return print("Hunger: " + str(self.hunger))
 
     
     def play(self):
-            self.hunger += 1
-            self.thirst += 1
-            return print("Hunger: " + str(self.hunger) + ", Thirst: " + str(self.thirst))
+        self.hunger += 1
+        self.thirst += 1
+        return print("Hunger: " + str(self.hunger) + ", Thirst: " + str(self.thirst))
 
 dog = Animal()
 cat = Animal()
