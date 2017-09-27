e6705deef754f5ce3e3c95a3289dce0efa58bafb
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-27 16:35:27 +0200

    feat(fibonacci,letter_counter): complete

--- Week-04/day-03/peter_keller_test.py
+++ Week-04/day-03/peter_keller_test.py
@@ -7,20 +7,34 @@ class AppleTest(unittest.TestCase):
     def test_get_apple(self):
         self.assertEqual(test_method.get_apple(), "apple")
 
+if __name__ == "__main__":
+    unittest.main(exit=False)
 
 class SumElements(unittest.TestCase):
     def test_sum_of_elements(self):
         self.assertEqual(test_method.sum_of_elements([12, 1]), 13)
 
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+class AnagramTest(unittest.TestCase):
+    def test_anagram(self):
+        self.assertTrue(test_method.anagram("ter", "ret"))
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(exit=False)
+
+class CountLetterTest(unittest.TestCase):
+    def test_count_letters(self):
+        self.assertEqual(test_method.count_letters("aaacc"),{"a" : 3, "c" : 2})
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+class FibonacciTest(unittest.TestCase):
+    def test_fibonacci(self):
+        self.assertEqual(test_method.fibonacci(10),55)
 
 
-#class AnagramTest(unittest.TestCase):
-#    def test_anagram(self):
-#        self.assertEqual()
-
-
-#if __name__ == "__main__":
-#    unittest.main()
+if __name__ == "__main__":
+    unittest.main(exit=False)
--- Week-04/day-03/peter_keller_work.py
+++ Week-04/day-03/peter_keller_work.py
@@ -6,9 +6,20 @@ class Tasks(object):
     def sum_of_elements(self, list_of_numbers):   
         return sum(list_of_numbers)
 
-#    def anagram(self, text_one, text_two):
-#        if sorted(text_one) == sorted(text_two):
-#            return True
-#        else:
-#            return False
+    def anagram(self, text_one, text_two):
+        if sorted(text_one) == sorted(text_two):
+            return True
+        else:
+            return False
 
+    def count_letters(self, word):
+        dictionary = {}
+        for characters in list(word):
+            dictionary[characters] = word.count(characters)
+        return dictionary
+            
+    def fibonacci(self, number):
+        a,b = 1,1
+        for i in range(number-1):
+            a, b = b, a + b
+        return a
