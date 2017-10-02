d1d13c8354712c64aadfba12966c041ca3d5555b
kellykeller1213 <keller.f.peter@gmail.com>
2017-10-02 16:44:10 +0200

    feat(rpg_game): character on map, renamed map file

--- Week-05/day-01/game.py
+++ Week-05/day-01/game.py
@@ -1,30 +1,41 @@
 from tkinter import *
 from PIL import Image, ImageTk
-from thingy import Map
+from map_file import Map
 
 
-size = 720
 root = Tk()
+size = 720
 map = Map()
-image1 = PhotoImage(file = 'floor.png')
-image2 = PhotoImage(file = 'wall.Png')
-
 root.configure(background ='black')
 canvas = Canvas(root, width=size, height=size,bg="white",bd=0)
 canvas.pack()
-
-
-for y in range(len(map.map)):
-    for x in range(len(map.map)):
-        if map.map[y][x] == 0:
-            canvas.create_image(x*72, y*72, anchor = NW, image = image1)
-        elif map.map[y][x] == 1:
-            canvas.create_image(x*72, y*72, anchor = NW, image = image2)
+class DrawMap():
+    def __init__(self):
+        self.image1 = PhotoImage(file = 'floor.png')
+        self.image2 = PhotoImage(file = 'wall.Png')
+        self.draw_map()
         
+    def draw_map(self):
+        for y in range(len(map.map)):
+            for x in range(len(map.map)):
+                if map.map[y][x] == 0:
+                    canvas.create_image(x*72, y*72, anchor = NW, image = self.image1)
+                elif map.map[y][x] == 1:
+                    canvas.create_image(x*72, y*72, anchor = NW, image = self.image2)
+        
+class DrawEntity:
+    def __init__(self):
+        self.hero = PhotoImage(file = "hero-down.png")
+        self.drawing_entity()
+    
+    def drawing_entity(self):
+        canvas.create_image(0, 0, anchor = NW, image = self.hero)
 
+    
 canvas.pack()
 
-# Select the canvas to be in focused so it actually recieves the key hittings
+draw_map = DrawMap()
+drawing_entity = DrawEntity()
 canvas.focus_set()
 
 root.mainloop()
