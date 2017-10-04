a5dfa0a3c7fb40ee85c038b844e01d91a528bb50
kellykeller1213 <keller.f.peter@gmail.com>
2017-10-04 11:38:31 +0200

    feat(rpg_game): turning hero

--- Week-05/day-01/game.py
+++ Week-05/day-01/game.py
@@ -23,41 +23,47 @@ class DrawMap():
                 elif map.map[y][x] == 1:
                     canvas.create_image(x*72, y*72, anchor = NW, image = self.image2)
         
-class DrawEntity:
-    def __init__(self):
-        self.hero = PhotoImage(file = "hero-down.png")
-        self.drawing_entity()
-    
-    def drawing_entity(self):
-        self.put_entity = canvas.create_image(0, 0, anchor = NW, image = self.hero)
-
     
 
 class Entity(object):
     def __init__(self):
-        pass
+        self.hero_down = PhotoImage(file = "hero-down.png")
+        self.hero_up = PhotoImage(file = "hero-up.png")
+        self.hero_left = PhotoImage(file = "hero-left.png")
+        self.hero_right = PhotoImage(file = "hero-right.png")
+        self.drawing_entity()
         
     def move(self, dx, dy):
         canvas.move(hero.put_entity, dx, dy )
+    
+    def update_costume(self, costume):
+        self.costume = costume
+        canvas.itemconfigure(self.put_entity, image=self.costume)
 
-entities = Entity()
-def moving(e):
-    if (e.keysym == 'Up'):
-        entities.move(0,-72)
-    elif(e.keysym == 'Down'):
-        entities.move(0,72)
-    elif(e.keysym == 'Left'):
-        entities.move(-72,0)
-    elif(e.keysym == 'Right'):
-        entities.move(72,0)
+    
+    def drawing_entity(self):
+        self.put_entity = canvas.create_image(0, 0, anchor = NW, image = self.hero_down)
 
 
-root.bind("<KeyPress>", moving)
+    def moving(self, e):
+        if (e.keysym == 'Up'):
+            self.move(0,-72)
+            self.update_costume(self.hero_up)
+        elif(e.keysym == 'Down'):
+            self.move(0,72)
+            self.update_costume(self.hero_down)
+        elif(e.keysym == 'Left'):
+            self.move(-72,0)
+            self.update_costume(self.hero_left)
+        elif(e.keysym == 'Right'):
+            self.move(72,0)
+            self.update_costume(self.hero_right)
 
 canvas.pack()
 
 draw_map = DrawMap()
-hero = DrawEntity()
+hero = Entity()
+root.bind("<KeyPress>", hero.moving)
 canvas.focus_set()
 
 root.mainloop()
