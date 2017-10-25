e57b8a9e324a545fa9776025074cc4f9283de58c
kellykeller1213 <keller.f.peter@gmail.com>
2017-10-25 18:47:54 +0200

    feat(hello_others): complete

---
+++ Week-07/day-01/03 hello-others/hello_others.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello Ati");
+console.log("Hello Zsuzsi");
+console.log("Hello Attila");
