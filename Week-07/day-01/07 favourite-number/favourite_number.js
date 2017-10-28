fdf0665470b273ab58b0e59b29da335b852a840a
peter-keller <keller.f.peter@gmail.com>
2017-10-28 19:50:01 +0200

    feat(favourite_number): complete

---
+++ Week-07/day-01/07 favourite-number/favourite_number.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+
+
+var favoriteNumber = 8
+
+console.log("My favourite number is: " + favoriteNumber);
+// Store your favorite number in a variable (as a number)
+
+// And print it like this: 'My favorite number is: 8
