bf49267d520cf20fb6554a9485264f82b5f64327
peter-keller <keller.f.peter@gmail.com>
2017-10-28 20:00:37 +0200

    feat(define_basic_info): complete

---
+++ Week-07/day-01/10 define-basic-info/define_basic_info.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+let name = "Peti"
+let age = 23
+let height = 1.74
+let married = false
+
+console.log("I am " + name + " and I am " + age + " years old and " + height + " cm tall and I would say I am married but that would be " + married)
+
+// Define several things as a variable, then print their values
+
+// Your name as a string
+
+// Your age as an integer
+
+// Your height in meters as a float
+
+// Whether you are married or not as a boolean
