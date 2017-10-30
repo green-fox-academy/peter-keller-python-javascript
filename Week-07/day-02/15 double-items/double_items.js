6f03b2ca111e2f9b2f3839fb06f331324f63f5b0
peter-keller <keller.f.peter@gmail.com>
2017-10-30 13:26:59 +0100

    feat(double_items): complete

---
+++ Week-07/day-02/15 double-items/double_items.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+// - Create an array variable named `ag` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+
+// - Double all the strings in the array, use a built in array method instead of a loop
+
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+var ag = ["Gin", "Whiskey", "Wine", "Beer"];
+
+ag = ag.map(function(x) {
+    return x + x;
+})
+
+console.log(ag)
