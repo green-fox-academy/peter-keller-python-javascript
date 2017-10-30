26a67feca2937d54bf86345c5f1b9e959fba4c0d
peter-keller <keller.f.peter@gmail.com>
2017-10-30 09:44:01 +0100

    feat(doubling): complete

---
+++ Week-07/day-02/01 doubling/doubling.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+// - Create a variable named `ak` and assign the value `123` to it
+
+// - Create a function called `doubling` that doubles it's input parameter
+
+// - Log the result of `doubling(ak)` to the console
+
+
+var ak = 123;
+
+function doubling(number) {
+    return ak *= 2
+}
+
+console.log(doubling(ak))
