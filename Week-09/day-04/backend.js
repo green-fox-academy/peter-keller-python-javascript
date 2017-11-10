56159382a7ffa99e83a85f3c394af1445ac83d28
kellykeller1213 <keller.f.peter@gmail.com>
2017-11-10 14:40:12 +0100

    feat(bookstore) lists all books with their information in a table

--- Week-09/day-04/backend.js
+++ Week-09/day-04/backend.js
@@ -1,26 +1,28 @@
 'use strict';
 
+
 const mysql = require('mysql');
 const express = require('express');
 const app = express();
+var books = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id;';
+
 express.json.type = 'application/json';
 
 app.use(express.json());
 
-
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ridata94',
+    host: "localhost",
+    user: "root",
+    password: "ridata94",
     database: 'bookstore'
 });
 
-connection.connect(function(err) {
+connection.connect(function(err){
     if (err) {
-      console.log(err, "error");
-    return
+        console.log("Error connection to database");
+    return;
     }
-    console.log('mysql server connected');
+    console.log("Connection estabilished");
 });
 
 app.get('/', function(request, response) {
@@ -28,19 +30,36 @@ app.get('/', function(request, response) {
 });
 
 app.get('/list', function(request, response) {
-    connection.query('SELECT book_name FROM book_mast;', function(err, rows){
-        if(err){
+    connection.query(books, function(err, rows) {
+        if (err) {
             console.log(err.toString());
+            return;
     };
-    console.log("Data received from Db:\n");
+    console.log("Data received from database");
     let htmlString = '<ul>';
     rows.forEach(function(row) {
-        htmlString = htmlString + '<li>' + row.book_name + '</li>';
+        htmlString = htmlString + "<li>" + row.book_name + "</li>";
     });
-    htmlString = htmlString + '</ul>';
+    htmlString = htmlString + "</ul>";
     response.send(htmlString);
     });
+
 });
 
+app.get('/books', function(request, response) {
+    connection.query(books, function(err, rows) {
+        if (err) {
+            console.log(err.toString());
+            return;
+    };
+    console.log("Data received from database");
+    let htmlString = '<table>';
+    rows.forEach(function(row) {
+        htmlString += '<tr><td>' + row.book_name + '</td><td>' + row.aut_name + '</td><td>' + row.cate_descrip + '</td><td>' + row.pub_name + '</td><td>' + row.book_price + '</td></tr>';
+    });
+    htmlString = htmlString + '</table>';
+    response.send(htmlString)
+    });
+});
 
 app.listen(3000);
