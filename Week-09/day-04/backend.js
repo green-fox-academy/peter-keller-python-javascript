67f547a8357160f4c20c199edf6c74ea3d06fa21
kellykeller1213 <keller.f.peter@gmail.com>
2017-11-16 17:06:09 +0100

    feat(bookstore): backend reworked, filter functional when its a category

--- Week-09/day-04/backend.js
+++ Week-09/day-04/backend.js
@@ -7,12 +7,13 @@ const app = express();
 var books = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id ';
 
 express.json.type = 'application/json';
+
 app.use("/assets", express.static("assets"));
 app.use(express.json());
 
 
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "ridata94",
@@ -22,10 +23,10 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     if (err) {
-        console.log("Error connection to database");
-    };
-    console.log("Connection estabilished");
-});
+        console.log("Cannot connect to database");
+    } else { 
+        console.log("Connection estabilished");
+}});
 
 
 app.get('/', function(request, response) {
@@ -56,23 +57,33 @@ app.get('/booksdata', function(request, response) {
 
 
 app.get('/books', function(request, response) {
-    connection.query(books, function(err, rows) {
-        books = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id ';
-        
+    let dataLocation = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id ';    
+    if (request.query.category) {
+        dataLocation = dataLocation + ' WHERE cate_descrip = "' + request.query.category + '";';
+    } else if (request.query.publisher) {
+        console.log(request.query.publisher)
+        dataLocation = dataLocation + ' WHERE pub_id = ' + request.query.publisher + ';';
+    }
+    connection.query(dataLocation, function(err, rows) {
         if (err) {
             console.log(err.toString());
-        } else if (request.query.category) {
-            books = books + 'WHERE cate_descrip = "' + request.query.category + '";';
         }
-        let result = '<ul>'
-        rows.forEach(function(element) {
-            result = result += '<li>' + element.book_name + '</li>' ;
-        });
-        result = result + '</ul>';
+        response.send(rows);
         console.log("Selected data received from database");
-
-        response.send(result);
+        console.log(rows)
     });
 });
-
+/*
+function getQueryVariable(anythin) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', request.query);
+}
+*/
 app.listen(3000);
