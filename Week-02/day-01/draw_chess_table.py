12ae9c328785b0faf1ea32b106c983c7d0dbea0a
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-25 19:56:41 +0100

    feat(draw_chess_table): finished

--- Week-02/day-01/draw_chess_table.py
+++ Week-02/day-01/draw_chess_table.py
@@ -9,3 +9,18 @@
 # % % % % 
 #  % % % %
 #
+character = ""
+for i in range(0, 8):
+    for j in range(0, 8):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                character += "%"
+            else:
+                character += " "
+        else:
+            if j % 2 == 0:
+                character += " "
+            else:
+                character += "%"
+    print (character + "\n")
+    character = ""
