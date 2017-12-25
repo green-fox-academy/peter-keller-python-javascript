877c0f86114952839d092d52c704bd76c3884773
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-25 20:15:00 +0100

    refact(mile_to_km): following pep8

--- Week-02/day-01/mile_to_km_converter.py
+++ Week-02/day-01/mile_to_km_converter.py
@@ -1,4 +1,5 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
-distance = input("How many kms?")
+
+distance = int(input("Kilometers: "))
 print(distance * 1.6)
