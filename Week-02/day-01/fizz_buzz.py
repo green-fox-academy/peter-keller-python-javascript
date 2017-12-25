7420cbf0dff7b1dcb8ab501058d30bb89a243013
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-25 20:00:52 +0100

    refact(fizz_buzz): following pep8

--- Week-02/day-01/fizz_buzz.py
+++ Week-02/day-01/fizz_buzz.py
@@ -2,12 +2,12 @@
 # But for multiples of three print �Fizz� instead of the number
 # and for the multiples of five print �Buzz�.
 # For numbers which are multiples of both three and five print �FizzBuzz�.
-for number in range (0,100):
-    if number%3 == 0 and number%5 == 0:
-        print ("FizzBuzz")    
-    elif number%5 == 0:
+for number in range (0, 100):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")    
+    elif number % 5 == 0:
         print("Buzz")
-    elif number%3 == 0:
+    elif number % 3 == 0:
         print("Fizz")
     else:
         print(number)
