a48590ebc951ec065b8f483ac25eac8eca488cd3
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 17:27:52 +0100

    refact(odd_even): following pep8

--- Week-02/day-01/odd_even.py
+++ Week-02/day-01/odd_even.py
@@ -1,5 +1,6 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
+
 number = int(input("Odd or even? Let's find out! "))
 if number % 2 == 0:
     print("even")
