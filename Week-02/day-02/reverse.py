1cb761b6dc2231f7a835920297054f336f278b24
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 18:05:11 +0100

    refact(reverse): following pep8

--- Week-02/day-02/reverse.py
+++ Week-02/day-02/reverse.py
@@ -2,6 +2,8 @@
 #   with the following content: `[3, 4, 5, 6, 7]`
 # - Reverse the order of the elements in `aj`
 # - Print the elements of the reversed `aj`
+
 aj = [3, 4, 5, 6, 7]
 aj = aj[::-1]
+
 print(aj)
