e11de21bacdbc85ebd5f41cd990ffa913cdd54b9
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 17:55:54 +0100

    refact(factorio): following pep8

--- Week-02/day-02/factorio.py
+++ Week-02/day-02/factorio.py
@@ -2,11 +2,12 @@
 #   that returns it's input's factorial
 n = int(input("Insert number: "))
 
+
 def factorio(num):
     g = 1
-    for i in range(1,(num + 1)):
+    for i in range(1, (num + 1)):
         g = g * i
     print("Factorio: ", g)
 
-factorio(n)
 
+factorio(n)
