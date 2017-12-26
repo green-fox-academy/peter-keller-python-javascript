019146d48ce1a90e13b826456b5b77cb03a78cfe
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 18:13:29 +0100

    refact(append_letter): following pep8

--- Week-02/day-03/append_letter.py
+++ Week-02/day-03/append_letter.py
@@ -1,14 +1,18 @@
-# Create a function called 'create_new_verbs()' which takes a list of verbs and a string as parameters
+# Create a function called 'create_new_verbs()' which takes a list
+# of verbs and a string as parameters
 # The string shouldf be a preverb
-# The function appends every verb to the preverb and returns the list of the new verbs
+# The function appends every verb to the preverb and
+# returns the list of the new verbs
 
 
 verbs = ["megy", "ver", "kapcsol", "rak", "néz"]
 preverb = "be"
 
+
 def new_verbs(preverb):
     for i in range(len(verbs)):
         mod_verb = preverb + verbs[i]
         print(mod_verb)
+
 
 new_verbs(preverb)
