3850bac65dd7fa7a357f3a49d67c163b07411b5b
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 18:36:34 +0100

    refact(unique): following pep8

--- Week-02/day-04/unique.py
+++ Week-02/day-04/unique.py
@@ -1,10 +1,12 @@
 numbers = [1, 11, 34, 11, 52, 61, 1, 34]
 
-def remove_duplicates(x):
+
+def remove_duplicates():
     a = []
     for i in numbers:
         if i not in a:
             a.append(i)
     return a
 
-print(remove_duplicates(1))
+
+print(remove_duplicates())
