91696c32bd76efe0780c32d2cb721ae723bd09ab
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 18:38:56 +0100

    refact(anagram): following pep8

--- Week-02/day-05/anagram.py
+++ Week-02/day-05/anagram.py
@@ -1,9 +1,12 @@
 word1_input = input("Insert info: ")
 word2_input = input("Another info: ")
 
-def anagram_length(word1 , word2):
+
+def anagram_length(word1, word2):
     if sorted(word1) == sorted(word2):
-        print("Guess I am an anagram, no big deal")
+        print("Brace yourself, anagram detected")
     else:
-        print("Move along, nothing to see here")
+        print("Not an anagram, nothing to see here")
+
+
 anagram_length(word1_input, word2_input)
