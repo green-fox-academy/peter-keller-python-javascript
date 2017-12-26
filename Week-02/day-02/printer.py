670030e115be1ce3a27636996dd4062ca63c491d
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 18:04:26 +0100

    refact(printer): following pep8

--- Week-02/day-02/printer.py
+++ Week-02/day-02/printer.py
@@ -1,7 +1,10 @@
 # - Create a function called `printer`
 #   which prints the input parameters
 #   (can have multiple number of arguments)
+
+
 def printer(parameter):
     print(parameter)
+
 
 printer("Peti")
