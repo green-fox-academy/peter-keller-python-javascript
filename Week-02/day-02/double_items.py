50f9b48c38c4f02878df084db5458ceb5b574ae2
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 17:53:11 +0100

    refact(double_items): following pep8

--- Week-02/day-02/double_items.py
+++ Week-02/day-02/double_items.py
@@ -2,6 +2,6 @@
 #   with the following content: `[3, 4, 5, 6, 7]`
 # - Double all the values in the array
 ag = [3, 4, 5, 6, 7]
-for i in range(0,len(ag)):
+for i in range(0, len(ag)):
     ag[i] *= 2
     print(ag[i])
