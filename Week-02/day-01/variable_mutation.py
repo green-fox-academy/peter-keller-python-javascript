075b5ed191687a9c9a6a1e4c08e9563f6dfb5f26
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-26 17:37:44 +0100

    refact(var_mutation): following pep8

--- Week-02/day-01/variable_mutation.py
+++ Week-02/day-01/variable_mutation.py
@@ -5,16 +5,11 @@ a += 10
 print(a)
 
 
-
-
 b = 100
 # make it smaller by 7
-b -=7
-
+b -= 7
 
 print(b)
-
-
 
 
 c = 44
@@ -24,15 +19,11 @@ c *= 2
 print(c)
 
 
-
-
 d = 125
 # please divide by 5 d's value 
 d /= 5
 
 print(d)
-
-
 
 
 e = 8
@@ -42,8 +33,6 @@ e= e ** 2
 print(e)
 
 
-
-
 f1 = 123
 f2 = 345
 # tell if f1 is bigger than f2 (pras a boolean)
@@ -51,8 +40,6 @@ if f1 > f2:
     print(True)
 else:
     print(False)
-
-
 
 
 g1 = 350
@@ -66,31 +53,27 @@ else:
 
 h = 1357988018575474
 # tell if it has 11 as a divisor (pras a boolean)
-if (h%11) ==0:
+if (h % 11) == 0:
     print(True)
 else:
-    (False)
-
-
+    print(False)
 
 
 i1 = 10
 i2 = 3
 # tell if i1 is higher than i2 squared and smaller than i2 cubed (pras a boolean)
-if i1 > i2 **2 and i1 < i2 ** 3:
+if i1 > i2 ** 2 and i1 < i2 ** 3:
     print(True)
 else:
     print(False)
-
 
 
 j = 1521
 # tell if j is dividable by 3 or 5 (pras a boolean)
-if j%3 == 0 or j%5 == 0:
+if j % 3 == 0 or j % 5 == 0:
     print(True)
 else:
     print(False)
-
 
 
 k = "Apple"
