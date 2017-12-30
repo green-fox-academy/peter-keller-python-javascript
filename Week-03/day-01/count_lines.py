01f1f7e4ddadfeabada669f4e66ee2b32235e799
kellykeller1213 <keller.f.peter@gmail.com>
2017-12-30 17:48:32 +0100

    feat(count_lines): replaced file open to with method

--- Week-03/day-01/count_lines.py
+++ Week-03/day-01/count_lines.py
@@ -2,13 +2,17 @@
 # then returns the number of lines the file contains.
 # It should return zero if it can't open the file, and
 # should not raise any error.
+
+file_to_read = "day-01/single-chars.txt"
+
+
 def count_lines():
     try:
-        f = open("my-file.txt", "r")
-        num_lines = sum(1 for line in open('my-file.txt'))
-        print(num_lines)
+        with open(file_to_read, 'r') as file:
+            num_lines = sum(1 for line in file)
+        return num_lines
     except FileNotFoundError:
-        print("Unable to read file: my-file.txt")
+        return "Unable to read file: " + file_to_read
 
-count_lines()
 
+print(count_lines())
